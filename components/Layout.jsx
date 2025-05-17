d47d591ddc189baf3504fb3446f78e36e16ee0a7
Musab Zahid Ansari <musabzahida@gmail.com>
2025-05-17 15:30:30 +0530

    added .env to gitignore, fixed import issue

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Head from 'next/head';
 import Navbar from './Navbar';
 import Footer from './Footer';
-import LeaderBoard from './leaderBoard';
+import LeaderBoard from './LeaderBoard';
 import { useStateContext } from '@/context/StateContext';
 
 const Layout = ({children}) => {
